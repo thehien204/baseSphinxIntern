403f0ca0f62215d92a69b266f83583f12a9a8faf
hien <phamthehienc20042000@gmail.com>
2023-07-06 11:34:13 +0700

    1

---
+++ src/main/java/com/example/basesphinx/BaseSphinxApplication.java
@@ -0,0 +1,13 @@
+package com.example.basesphinx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseSphinxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseSphinxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/basesphinx/BaseSphinxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basesphinx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseSphinxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
